88760b465a22174a71e0e187ef7daf88d72d18fa
William Carty <will.carty@interapthq.com>
2016-09-16 09:40:42 -0400

    updated borders and check list

---
+++ test.js
@@ -0,0 +1,10 @@
+var userAnswer =1
+if(userAnswer <= 1){
+    console.log ("You and  Bieber start racing. It's neck and neck! You win by a shoelace");
+}
+    else
+    {
+        console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+
+    }
+
